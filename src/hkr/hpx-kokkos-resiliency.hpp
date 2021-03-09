1d8b74ef7d12c7148a0b415e58d57e3d2a283803
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2021-03-09 16:08:19 +0530

    Remove bloat

--- src/hkr/hpx-kokkos-resiliency.hpp
+++ src/hkr/hpx-kokkos-resiliency.hpp
@@ -10,65 +10,6 @@
 #include <memory>
 
 namespace hpx { namespace kokkos { namespace resiliency {
-    namespace detail {
-
-        template <typename Result, typename Pred, typename F, typename Tuple>
-        struct async_replay_executor_helper
-          : std::enable_shared_from_this<
-                async_replay_executor_helper<Result, Pred, F, Tuple>>
-        {
-            template <typename Pred_, typename F_, typename Tuple_>
-            async_replay_executor_helper(Pred_&& pred, F_&& f, Tuple_&& tuple)
-              : pred_(std::forward<Pred_>(pred))
-              , f_(std::forward<F_>(f))
-              , tuple_(std::forward<Tuple_>(tuple))
-            {
-            }
-
-            template <typename Executor>
-            decltype(auto) call(Executor&& exec, std::size_t n)
-            {
-                auto f = this->f_;
-                auto pred = this->pred_;
-                auto tuple = this->tuple_;
-
-                return hpx::async(
-                    exec, KOKKOS_LAMBDA() {
-                        for (std::size_t i = 0u; i < n; ++i)
-                        {
-                            Result res =
-                                hpx::util::invoke_fused_r<Result>(f, tuple);
-
-                            bool result = pred(res);
-
-                            if (result)
-                            {
-                                return res;
-                            }
-                        }
-                    });
-            }
-
-            Pred pred_;
-            F f_;
-            Tuple tuple_;
-        };
-
-        template <typename Result, typename Pred, typename F, typename... Ts>
-        std::shared_ptr<async_replay_executor_helper<Result,
-            typename std::decay<Pred>::type, typename std::decay<F>::type,
-            std::tuple<typename std::decay<Ts>::type...>>>
-        make_async_replay_executor_helper(Pred&& pred, F&& f, Ts&&... ts)
-        {
-            using result_t = async_replay_executor_helper<Result,
-                typename std::decay<Pred>::type, typename std::decay<F>::type,
-                std::tuple<typename std::decay<Ts>::type...>>;
-
-            return std::make_shared<result_t>(std::forward<Pred>(pred),
-                std::forward<F>(f), std::make_tuple(std::forward<Ts>(ts)...));
-        }
-
-    }    // namespace detail
 
     template <typename Executor, typename Pred, typename F, typename... Ts,
         HPX_CONCEPT_REQUIRES_(
@@ -78,13 +19,26 @@ namespace hpx { namespace kokkos { namespace resiliency {
     tag_invoke(async_replay_validate_t, Executor&& exec, std::size_t n,
         Pred&& pred, F&& f, Ts&&... ts)
     {
+        // Generate necessary components
         using result_t =
             typename hpx::util::detail::invoke_deferred_result<F, Ts...>::type;
+        auto tuple = hpx::make_tuple(std::forward<Ts>(ts)...);
 
-        auto helper = detail::make_async_replay_executor_helper<result_t>(
-            std::forward<Pred>(pred), std::forward<F>(f),
-            std::forward<Ts>(ts)...);
+        return hpx::async(
+            exec, KOKKOS_LAMBDA() {
+                for (std::size_t i = 0u; i < n; ++i)
+                {
+                    result_t res =
+                        hpx::util::invoke_fused_r<result_t>(f, tuple);
 
-        return helper->call(std::forward<Executor>(exec), n);
+                    bool result = pred(res);
+
+                    if (result)
+                    {
+                        return res;
+                    }
+                }
+            });
     }
+
 }}}    // namespace hpx::kokkos::resiliency
