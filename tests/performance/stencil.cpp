8b40659a7354c5d1650e99454f7e17536e75ca71
Nikunj Gupta <ngupta@diablo.rostam.cct.lsu.edu>
2021-05-31 09:08:28 -0500

    Partially working stencil code

--- tests/performance/stencil.cpp
+++ tests/performance/stencil.cpp
@@ -1,228 +1,193 @@
-// #include <hpx/kokkos.hpp>
+#include <hpx/kokkos.hpp>
 
-// #include <hpx/kokkos/detail/polling_helper.hpp>
-// #include <hkr/executor/returning-executor.hpp>
-// #include <hkr/hpx-kokkos-resiliency-executor.hpp>
-// #include <hkr/hpx-kokkos-resiliency.hpp>
+#include <hpx/kokkos/detail/polling_helper.hpp>
+#include <hkr/kokkos-executor.hpp>
 
-// #include <hpx/modules/synchronization.hpp>
-// #include <boost/program_options.hpp>
+#include <boost/program_options.hpp>
 
-// #include <random>
+#include <random>
 
-// namespace stencil {
+struct validate
+{
+    HPX_HOST_DEVICE bool operator()(hpx::tuple<double, double> result) const
+    {
+        return true;
+    }
+};
 
-//     // HPX_HOST_DEVICE double const checksum_tol = 1.0e-10;
+HPX_HOST_DEVICE double stencil(double left, double center, double right)
+{
+    return 0.5 * (0.75) * left + (0.75) * center - 0.5 * (0.25) * right;
+}
 
-//     template <typename ExecutionSpace>
-//     struct partition_data;
+HPX_HOST_DEVICE double left_flux(double left, double center)
+{
+    return (0.625) * left - (0.125) * center;
+}
 
-//     template <typename ExecutionSpace>
-//     using partition = hpx::shared_future<partition_data<ExecutionSpace>>;
+HPX_HOST_DEVICE double right_flux(double center, double right)
+{
+    return 0.5 * (0.75) * center + (1.125) * right;
+}
 
-//     template <typename ExecutionSpace>
-//     using space = Kokkos::View<partition<ExecutionSpace>*, ExecutionSpace>;
+int main(int argc, char* argv[])
+{
+    Kokkos::initialize(argc, argv);
 
-//     template <typename ExecutionSpace>
-//     struct partition_data
-//     {
-//         using execution_space = ExecutionSpace;
+    {
+        hpx::kokkos::detail::polling_helper helper;
 
-//         HPX_HOST_DEVICE partition_data(std::uint64_t size)
-//           : data_("subdomain_space", size)
-//           , size_(size)
-//           , checksum_(0.0)
-//           , test_value_(0.0)
-//         {
-//         }
+        namespace bpo = boost::program_options;
+        bpo::options_description desc("Options");
 
-//         HPX_HOST_DEVICE partition_data(std::uint64_t subdomain_width,
-//             double subdomain_index, std::uint64_t subdomains)
-//           : data_("subdomain_space", subdomain_width + 1)
-//           , size_(subdomain_width + 1)
-//           , test_value_(0.0)
-//         {
-//             constexpr double pi = 3.1415926535;
+        desc.add_options()("n-value",
+            bpo::value<std::uint64_t>()->default_value(3),
+            "Number of repeat launches for async replay.");
+        desc.add_options()("error-rate",
+            bpo::value<std::uint64_t>()->default_value(1),
+            "Error rate for injecting errors.");
+        desc.add_options()("subdomain-width",
+            bpo::value<std::uint64_t>()->default_value(128),
+            "Local x dimension (of each partition).");
+        desc.add_options()("iterations",
+            bpo::value<std::uint64_t>()->default_value(10),
+            "Number of time steps.");
+        desc.add_options()("steps-per-iteration",
+            bpo::value<std::uint64_t>()->default_value(16),
+            "Number of time steps per iterations.");
+        desc.add_options()("subdomains",
+            bpo::value<std::uint64_t>()->default_value(10),
+            "Number of partitions.");
 
-//             checksum_ = 0.0;
-//             for (std::uint64_t k = 0; k != subdomain_width + 1; ++k)
-//             {
-//                 data_[k] = std::sin(2 * pi *
-//                     ((0.0 + subdomain_width * subdomain_index + k) /
-//                         static_cast<double>(subdomain_width * subdomains)));
-//                 checksum_ += data_[k];
-//             }
-//         }
+        bpo::variables_map vm;
 
-//         HPX_HOST_DEVICE partition_data(partition_data&& other)
-//           : data_(std::move(other.data_))
-//           , size_(other.size_)
-//           , checksum_(other.checksum_)
-//           , test_value_(other.test_value_)
-//         {
-//         }
+        // Setup commandline arguments
+        bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
+        bpo::notify(vm);
 
-//         // To support copying over KOKKOS_LAMBDA (Only shallow copy required)
-//         HPX_HOST_DEVICE partition_data(partition_data const& other)
-//           : data_(other.data_)
-//           , size_(other.size_)
-//           , checksum_(other.checksum_)
-//           , test_value_(other.test_value_)
-//         {
-//         }
+        // Start application work
+        std::uint64_t n = vm["n-value"].as<std::uint64_t>();
+        std::uint64_t error = vm["error-rate"].as<std::uint64_t>();
+        std::uint64_t subdomain_width =
+            vm["subdomain-width"].as<std::uint64_t>();
+        std::uint64_t iterations = vm["iterations"].as<std::uint64_t>();
+        std::uint64_t subdomains = vm["subdomains"].as<std::uint64_t>();
+        std::uint64_t sti = vm["steps-per-iteration"].as<std::uint64_t>();
 
-//         HPX_HOST_DEVICE double& operator[](std::uint64_t idx)
-//         {
-//             return data_[idx];
-//         }
-//         HPX_HOST_DEVICE double operator[](std::uint64_t idx) const
-//         {
-//             return data_[idx];
-//         }
+        {
+            Kokkos::Cuda cuda_inst{};
+            Kokkos::Experimental::HPX hpx_inst{};
 
-//         HPX_HOST_DEVICE std::uint64_t size() const
-//         {
-//             return size_;
-//         }
+            Kokkos::View<double** [2], Kokkos::CudaSpace> data_device(
+                "data_device", subdomains, subdomain_width + 1);
+            Kokkos::View<double** [2], Kokkos::Experimental::HPX> data_host(
+                "data_host", subdomains, subdomain_width + 1);
 
-//         HPX_HOST_DEVICE double checksum() const
-//         {
-//             return checksum_;
-//         }
+            Kokkos::View<double*, Kokkos::CudaSpace> checksum_device(
+                "checksum_device", subdomains);
+            Kokkos::View<double*, Kokkos::Experimental::HPX> checksum_host(
+                "checksum_host", subdomains);
 
-//         HPX_HOST_DEVICE void set_checksum()
-//         {
-//             checksum_ = std::accumulate(data_.begin(), data_.end(), 0.0);
-//         }
+            Kokkos::View<double*, Kokkos::CudaSpace> test_value_device(
+                "test_device", subdomains);
+            Kokkos::View<double*, Kokkos::Experimental::HPX> test_value_host(
+                "test_host", subdomains);
 
-//         HPX_HOST_DEVICE void set_test_value(double test_value)
-//         {
-//             test_value_ = test_value;
-//         }
+            const double pi = std::acos(-1.0);
 
-//         HPX_HOST_DEVICE double verify_result() const
-//         {
-//             return std::abs(checksum_ - test_value_);
-//         }
+            // Initialize
+            Kokkos::parallel_for(
+                "initialize_data",
+                Kokkos::RangePolicy<Kokkos::Experimental::HPX>(
+                    hpx_inst, 0, subdomains),
+                KOKKOS_LAMBDA(std::uint64_t i) {
+                    checksum_host[i] = 0.0;
+                    test_value_host[i] = 0.0;
 
-//         HPX_HOST_DEVICE void resize(std::uint64_t size)
-//         {
-//             Kokkos::resize(data_, size);
-//             size_ = size;
-//         }
+                    for (std::uint64_t j = 0; j != subdomain_width + 1; ++j)
+                    {
+                        double value =
+                            static_cast<double>(subdomain_width * i + j) /
+                            static_cast<double>(subdomain_width * subdomains);
+                        data_host(i, j, 0) = std::sin(2 * pi * value);
 
-//     private:
-//         Kokkos::View<double*, execution_space> data_;
-//         std::uint64_t size_;
-//         double checksum_;
-//         double test_value_;
-//     };
+                        checksum_host[i] += data_host(i, j, 0);
+                    }
+                });
+            Kokkos::fence();
 
-//     template <typename ExecutionSpace, typename Executor>
-//     hpx::future<space<ExecutionSpace>> do_work(Executor& exec,
-//         std::uint64_t subdomains, std::uint64_t subdomain_width,
-//         std::uint64_t iterations, std::uint64_t sti, std::uint64_t nd,
-//         std::uint64_t n_value, std::uint64_t error,
-//         hpx::lcos::local::sliding_semaphore& sem)
-//     {
-//         using hpx::kokkos::resiliency::dataflow_replay_validate;
+            Kokkos::deep_copy(data_host, data_device);
+            Kokkos::deep_copy(checksum_host, checksum_device);
+            Kokkos::deep_copy(test_value_host, test_value_device);
 
-//         Kokkos::View<space<ExecutionSpace>*, ExecutionSpace> U(
-//             "stencil_space", 2);
-//         for (int i = 0; i != 2; ++i)
-//             Kokkos::resize(U[i], subdomains);
+            std::vector<hpx::future<hpx::tuple<double, double>>> tasks;
+            auto exec =
+                hpx::kokkos::experimental::resiliency::make_replay_executor(
+                    cuda_inst, 3, validate{});
+            for (std::uint64_t timestep = 0; timestep != iterations; ++timestep)
+            {
+                for (std::uint64_t subdomain_index = 0;
+                     subdomain_index != subdomains; ++subdomain_index)
+                {
+                    std::uint64_t size = subdomains + 2 * sti + 1;
+                    Kokkos::View<double*, Kokkos::CudaSpace> workspace(
+                        "temp", size);
+                    hpx::future<hpx::tuple<double, double>> fut = hpx::async(
+                        exec, KOKKOS_LAMBDA() {
+                            std::uint64_t curr = timestep % 2;
+                            std::uint64_t next = (timestep + 1) % 2;
 
-//         hpx::for_loop(
-//             hpx::kokkos::kok.on(exec), 0u, subdomains,
-//             KOKKOS_LAMBDA(std::uint64_t i) {
-//                 U[0][i] = hpx::async(
-//                     exec, KOKKOS_LAMBDA() {
-//                         return partition_data<ExecutionSpace>(
-//                             subdomain_width, double(i), subdomains);
-//                     });
-//             });
-//     }
+                            for (std::uint64_t i = sti, j = 0; i != 0; --i, ++j)
+                                workspace[j] = data_device(
+                                    (subdomain_index + subdomains - 1) %
+                                        subdomains,
+                                    subdomain_width - sti, curr);
+                            for (std::uint64_t i = 0; i != subdomain_width; ++i)
+                                workspace[sti + i] =
+                                    data_device(subdomain_index, i, curr);
+                            for (std::uint64_t i = 0; i != sti; ++i)
+                                workspace[sti + subdomain_width + i] =
+                                    data_device(
+                                        (subdomain_index + 1) % subdomains, i,
+                                        curr);
 
-// }    // namespace stencil
+                            double checksum = 0.0;
+                            for (std::uint64_t i = 0; i != size; ++i)
+                                checksum += workspace[i];
 
-// int main(int argc, char* argv[])
-// {
-//     Kokkos::initialize(argc, argv);
+                            for (std::size_t t = 0; t != sti; ++t)
+                            {
+                                checksum -=
+                                    left_flux(workspace[0], workspace[1]);
+                                checksum -= right_flux(workspace[size - 2 * t],
+                                    workspace[size - 2 * t + 1]);
+                                for (std::size_t k = 0; k != size - 2 * t; ++k)
+                                    workspace[k] = stencil(workspace[k],
+                                        workspace[k + 1], workspace[k + 2]);
+                            }
 
-//     {
-//         hpx::kokkos::detail::polling_helper helper;
+                            double test_value = 0.0;
+                            for (std::uint64_t i = 0; i < subdomain_width + 1;
+                                 ++i)
+                            {
+                                data_device(subdomain_index, i, next) =
+                                    workspace[i];
+                                test_value += workspace[i];
+                            }
 
-//         hpx::kokkos::returning_executor exec_;
-//         hpx::kokkos::returning_host_executor host_exec_;
+                            test_value_device[subdomain_index] = test_value;
+                            checksum_device[subdomain_index] = checksum;
 
-//         namespace bpo = boost::program_options;
+                            return hpx::make_tuple(test_value, checksum);
+                        });
+                    tasks.emplace_back(std::move(fut));
+                }
+                hpx::wait_all(tasks);
+            }
+        }
+    }
 
-//         bpo::options_description desc("Options");
+    Kokkos::finalize();
 
-//         desc.add_options()("n-value",
-//             bpo::value<std::uint64_t>()->default_value(3),
-//             "Number of repeat launches for async replay.");
-//         desc.add_options()("error-rate",
-//             bpo::value<std::uint64_t>()->default_value(1),
-//             "Error rate for injecting errors.");
-//         desc.add_options()("subdomain-width",
-//             bpo::value<std::uint64_t>()->default_value(128),
-//             "Local x dimension (of each partition).");
-//         desc.add_options()("iterations",
-//             bpo::value<std::uint64_t>()->default_value(10),
-//             "Number of time steps.");
-//         desc.add_options()("steps-per-iteration",
-//             bpo::value<std::uint64_t>()->default_value(16),
-//             "Number of time steps per iterations.");
-//         desc.add_options()("subdomains",
-//             bpo::value<std::uint64_t>()->default_value(10),
-//             "Number of partitions.");
-//         desc.add_options()("nd", bpo::value<std::uint64_t>()->default_value(10),
-//             "Number of time steps to allow the dependency tree to grow to");
-
-//         bpo::variables_map vm;
-
-//         // Setup commandline arguments
-//         bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
-//         bpo::notify(vm);
-
-//         // Start application work
-//         std::uint64_t n = vm["n-value"].as<std::uint64_t>();
-//         std::uint64_t error = vm["error-rate"].as<std::uint64_t>();
-//         std::uint64_t subdomain_width =
-//             vm["subdomain-width"].as<std::uint64_t>();
-//         std::uint64_t iterations = vm["iterations"].as<std::uint64_t>();
-//         std::uint64_t subdomains = vm["subdomains"].as<std::uint64_t>();
-//         std::uint64_t sti = vm["steps-per-iteration"].as<std::uint64_t>();
-//         std::uint64_t nd = vm["nd"].as<std::uint64_t>();
-
-//         {
-//             std::cout << "Starting 1d stencil with dataflow replay validate"
-//                       << std::endl;
-
-//             // Measure execution time.
-//             std::uint64_t t = hpx::chrono::high_resolution_clock::now();
-
-//             {
-//                 // limit depth of dependency tree
-//                 hpx::lcos::local::sliding_semaphore sem(nd);
-
-//                 hpx::future<
-//                     typename stencil::space<Kokkos::DefaultExecutionSpace>>
-//                     result = stencil::do_work<Kokkos::DefaultExecutionSpace>(
-//                         exec_, subdomains, subdomain_width, iterations, sti, nd,
-//                         n, error, sem);
-
-//                 typename stencil::space<Kokkos::DefaultExecutionSpace>
-//                     solution = result.get();
-//                 hpx::wait_all(solution);
-//             }
-
-//             std::cout << "Time elapsed: "
-//                       << static_cast<double>(
-//                              hpx::chrono::high_resolution_clock::now() - t) /
-//                     1e9
-//                       << std::endl;
-//         }
-//     }
-// }
+    return 0;
+}
